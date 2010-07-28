ff7873481f8b37c5590cfeb7d6c4a01486fe216b
James Lin <jamesdlin@gmail.com>
2010-07-28 06:20:54 +0000

    Distribute dropt in .tar.gz form too
    Modify package.cmd to generate a .tar.gz file for dropt too, and change it
    to abort the script on any failure.
    
    Also, save README.html with Unix line-endings.

--- dropt/package.cmd
+++ dropt/package.cmd
@@ -5,29 +5,60 @@ setlocal
 if not defined DROPT_VERSION set DROPT_VERSION=%~1
 if "%DROPT_VERSION%" == "" (
     (echo.No version number specified.) 1>&2
+    (echo.Usage: %~n0 VERSION) 1>&2
     exit /b 1
 )
 
 :: Verify that everything builds.  This doesn't provide complete coverage,
 :: but it should be good enough.
 ::
-nmake /NOLOGO /f Makefile.vcwin32 all
-nmake /NOLOGO /f Makefile.vcwin32 DEBUG=1 all
-nmake /NOLOGO /f Makefile.vcwin32 _UNICODE=1 all
-nmake /NOLOGO /f Makefile.vcwin32 DROPT_NO_STRING_BUFFERS=1 all
-nmake /NOLOGO /f Makefile.vcwin32 DEBUG=1 _UNICODE=1 DROPT_NO_STRING_BUFFERS=1 all
 
-7z a -tzip -mx=9 -mcu=on -r -- "build\dropt-%DROPT_VERSION%.zip" ^
-    dropt.c ^
-    dropt.h ^
-    dropt_example.c ^
-    dropt_handlers.c ^
-    dropt_string.c ^
-    dropt_string.h ^
-    droptxx.cpp ^
-    droptxx.hpp ^
-    LICENSE ^
-    Makefile.gcc ^
-    Makefile.vcwin32 ^
-    README.html ^
-    test_dropt.c
+set MAKE=nmake /NOLOGO /f Makefile.vcwin32
+
+echo.*** Building dropt ***
+%MAKE% all
+if ERRORLEVEL 1 exit /b 1
+
+echo.*** Building dropt (DEBUG=1) ***
+%MAKE% DEBUG=1 all
+if ERRORLEVEL 1 exit /b 1
+
+echo.*** Building dropt (_UNICODE=1) ***
+%MAKE% _UNICODE=1 all
+if ERRORLEVEL 1 exit /b 1
+
+echo.*** Building dropt (DROPT_NO_STRING_BUFFERS=1) ***
+%MAKE% DROPT_NO_STRING_BUFFERS=1 all
+if ERRORLEVEL 1 exit /b 1
+
+echo.*** Building dropt (DEBUG=1 _UNICODE=1 DROPT_NO_STRING_BUFFERS=1) ***
+%MAKE% DEBUG=1 _UNICODE=1 DROPT_NO_STRING_BUFFERS=1 all
+if ERRORLEVEL 1 exit /b 1
+
+set FILES=dropt.c ^
+          dropt.h ^
+          dropt_example.c ^
+          dropt_handlers.c ^
+          dropt_string.c ^
+          dropt_string.h ^
+          droptxx.cpp ^
+          droptxx.hpp ^
+          LICENSE ^
+          Makefile.gcc ^
+          Makefile.vcwin32 ^
+          README.html ^
+          test_dropt.c
+
+set ZIP_FILE=build\dropt-%DROPT_VERSION%.zip
+set TAR_FILE=build\dropt-%DROPT_VERSION%.tar
+
+echo.*** Packaging %ZIP_FILE% ***
+7z a -tzip -mx=9 -mcu=on -r -- "%ZIP_FILE%" %FILES%
+if ERRORLEVEL 1 exit /b 1
+
+echo.*** Packaging %TAR_FILE%.gz ***
+tar -cv -f "%TAR_FILE%" %FILES%
+if ERRORLEVEL 1 exit /b 1
+
+gzip -f -9 "%TAR_FILE%"
+if ERRORLEVEL 1 exit /b 1
